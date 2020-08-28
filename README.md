b132161aa6d7a3da385bdf286db1395feeb7e3e3
Payton Banks <payton.banks@gmail.com>
2020-08-28 08:45:00 -0400

    Realigned landing page. Timer adjustment for smoother game play. Removal of redudndant/legacy code.

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 ![Game Site](https://cdn.pixabay.com/photo/2017/01/26/07/53/question-mark-2010011_1280.jpg)
 
 ## Description
+Quiz Game is a timed, multiple choice, mini-game application where the player answers a series of questions against a 60 second countdown timer. The player wins the game by answering all of the questions before running out of time. 
+
+The game opens up at the Quiz Game landing page that provides the player with instructions for the game. The player starts the game by clicking the 'Start Button' and will then be presented with the first question. Only one question and its multiple choice answers are presented per page view. Once an answer is submitted the player will then be prompted with a notification of "Correct" for a right answer and "Incorrect" for a wrong answer. Correct answers will award the player with one point and automatically advance the player to the next question, any incorrect answers will deduct 10 seconds from the remaining game play. 
+
+## Technology
+* HTML
+* CSS
+* Bootstrap
+* Commercial Free Image Use
+
+## Project Website
+
+https://klvvr.github.io/Quiz_Game/
+
--- script.js
+++ script.js
@@ -1,145 +1,221 @@
-///// GLBOBAL VARIABLES \\\\\
-/ Game End // Close // Save // Initials // High \\
-var startBtn = document.getElementById("startButton");
-var submitBtn = document.querySelector("submit");
-var saveBtn = document.querySelector("#save");
-
-var timerEl = document.getElementById("timer");
-
-
-var time = questions.length * 60;  // Assumes 10 seconds per ques.
-var questions = document.getElementById("quiz");
-var choices = document.getElementById("choices"); 
-
-/
-var highScoreClick = document.querySelector("highScoreClick")
-// Questions Array // Objects: /Choices /Answers \\
-var questionsForm = document.querySelector("#questionsFrom")
-var currentQuestion = document.querySelector("#currentQuestion")
-
-var questionOpt = docuement.querySelector("#questionOpt") //this is the "il"
-var currentId = 0;
-var questionIndex = 0; // This may be negative number per *class afterhours notes page12
-var playerChoice;
-
-
-///// HIGH SCORES \\\\\
-var playerInitials = document.querySelector("#playerInitials");
-var highScoreLog = document.querySelector("#highScoreLog")
-var highScores = []; // double check syntax
-var highScoreForm = document.querySelector("#highScoreForm")
-var questionBox = document.querySelector("#questionBox")
-var design2
-
-///// QUIZ QUESTIONS \\\\\
-
 var quizQuestions = [
     {
         question: "How do you print the working directory within the MAC terminal command line?",
-        choices: ["touch", "rm", "pwd", "git", "mv"],
+        choices: ["touch", "rm", "pwd", "git"],
         answer: "pwd"
     },
     
     {
         question: "What does an HTML document begin with?",
-        choices: ["The <end> tag", "a for loop", "iframe", "<!DOCTYPE html>", "body tag"],
+        choices: ["The <end> tag", "a for loop", "iframe", "<!DOCTYPE html>"],
         answer: "!DOCTYPE html>"
     },
     
     {
         question: "What is a hyperlink?",
-        choices: ["javascript", "CSS", "a color", "HTML", "table"],
+        choices: ["javascript", "CSS", "a color", "HTML"],
         answer: "HTML"
     },
     
     {
         question: "What is the largest heading tag in HTML?",
-        choices: ["h1", "h2", "h3", "h4", "h5"],
+        choices: ["h1", "h3", "h4", "h5"],
         answer: "h1"
     },
     
     {
         question: "Choose the semantic element below?",
-        choices: ["block", "blue", "<article>", "table", "svg"],
+        choices: ["blue", "<article>", "table", "svg"],
         answer: "<article>"
     }
     
 ];
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.getElementsByClassName("submitBtn")
 
- ///// TIMER \\\\\  
+var timerEl = document.getElementById("time");
+var submitScoreElement = document.querySelector("#submit-score");
+var userScoreElement = document.getElementById("user-score");
+var userNameInput;
+var questionHead = document.getElementById("questions");
+var choicesDiv = document.getElementById("choices");
+
+
+var questionNumber = 0;
+var time = quizQuestions.length * 15;
+
+var answer;
+
+var myInterval;
+
+function isGameOver () {
+    return ((time <= 0) || (questionNumber >= 5));
+}
+
+function setTimer() {
+    time--;
+    console.log(time)
+    timerEl.textContent = time;
+    
+    if (isGameOver()) {
+        // clearInterval(countdown);
+        // setTimeout(displayScore, 500);
+        alert('game over');
+
+        clearInterval(myInterval);
+        // remove current question
+        // display end of game -> clear the question, show 'you're a loser.', show the enter initials, 
+    }
+};
  
  //  Timer begins after the game has been initiated. The First question is then presented. //
-var timer = document.getElementById("timer");
-var currentTimer = 60;
-var outOfTime = document.querySelector("#outoftime")
-var score = 0; // Move to timer section 
-
-
 //  WHEN I click the start button
 function startTimer() {
-    event.preventDefault()
-    startBtn.style.display = "none"; //hide
-    
-    var timeInterval = setInterval(function () {
-        timerEl.textContent = "Time Remaining : " + currentTimer;
-        currentTimer--;
 
-        if  (currentTimer === 0) {
-            timer.textContent = "OUT OF TIME!";
-            currentQuestion.textContent = "";
-            optionsUl.style.display = "none";
-            savesScore(event); 
-            clearInterval(timeInterval);
-        
-        } else if (index > quizQuestions.length -1) {
-            timer.text.Content = "";
-            saveScore();
-            clearInterval(timeInterval);
-        }
-
-    }, 1000);
-    startQuiz(1);
+   // document.getElementById("home").classList.add('d-block'); // d-hide selector
+    //document.getElementById("quiz").classList.add('d-block'); // d-hide selector
+    startBtn.style.display = "none";
+    ///// 60 Second Timer \\\\\
+    myInterval = setInterval(setTimer, 1000)
 }
 
-var userChoices;
-function startQuiz()direction;
-
-    index += direction;
-    currentQuestion.textContecnt = """;
-    optionsUl.innerHTML = "";
-    currentQuestion.textContent = quiz(index).question;
-    userChoices = quiz[index].choices;
-
-    userChoices.forEach(function (option) {
-        var optionsLi = document.createElement("Li");
-        optionsLi.style.margin = "10px";
-        var optionsBtn = document.createElement("button");
-        option.style.width = "200px";
-
-        optionsLi.appendChild(optionBtn);
-        optionBtn.textContent = option;
-        questionsUl.appendChild(optionsUl);
-        optionsLi.addEventListener("click", (checkAnswer);
-
-
-
-    }); 
-
-
-}
-
-///// CHECKING FOR CORRECT ANSWER \\\\\
-
+// 
+// quizQuestions
+// +=====+=====+=====+=====+=====+  undefined
+// |  0  |  1  |  2  |  3  |  4  |
+// | q   | q   | q   | q   | q   |
+// | c   | c   | c   | c   | c   |
+// | a   | a   | a   | a   | a   |
+// +=====+=====+=====+=====+=====+
+//                            ^ 
+//                        questionNumber
 function checkAnswer(event) {
-    var element = event.target;
-    if (element.textContent) === quiz[index].answer) {
-        score++;
-        alert("Correct")
+    event.stopPropagation();
 
+    var userAnswer = event.srcElement.innerHTML;
+    var currentQuestion = quizQuestions[questionNumber];
+
+    console.log("The user answerd: " + userAnswer);
+    console.log("The correct answer is " + currentQuestion.answer);
+
+    if (currentQuestion.answer != userAnswer) {
+        time = time-10
+    }
+    choicesDiv.innerHTML = "";
+        questionNumber++; 
+    if (isGameOver()) {
+    
+        displayScore();
+    } else {
+        makeQuestion();
+    }
+}
+
+///////// Makes the qustion HTML for the current question \\\\\\\\\\
+function makeQuestion() {
+    questionHead.innerHTML = quizQuestions[questionNumber].question;
+    choicesDiv.innerHTML = "";
+    var list = document.createElement("ul");
+    list.setAttribute("class", "answer-list");
+    choicesDiv.appendChild(list);
+
+    var choices = quizQuestions[questionNumber].choices;
+
+    for (var q = 0; q < choices.length; q++) {
+        var wrapperDiv = document.createElement("li");    // <div></div>
+
+        var nextChoice = document.createElement("button");
+        nextChoice.setAttribute("value", choices[q]);
+        nextChoice.setAttribute("class", "btn btn-default");
+        nextChoice.textContent = choices[q];
+        nextChoice.addEventListener("click", checkAnswer);
+        wrapperDiv.appendChild(nextChoice); // <button>
+
+        list.appendChild(wrapperDiv); // <div><button/></div>
+        //answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    }
+}
+
+///// SCORE BOARD DISPLAY \\\\\
+function displayScore() {
+    document.getElementById("quiz").classList.remove('d-none'); // set to none
+    document.getElementById("submit-score").classList.remove('d-block'); // set to none
+    userScoreElement.textContent = "FINAL SCORE: " + secondsLeft + ".";
+}
+
+// function submit (event) {
+//     event.stopPropagation();
+//     addScore();
+    
+//     window.location.href = './high_scores.html'
+// };
+
+///// GAME START EVENT TIMERS \\\\
+// Don't forget to tturn this back on.
+//startBtn.addEventListener("click", startTimer);
+//submitBtn.addEventListener("click", submit)
+
+startBtn.addEventListener("click", function() { 
+    startTimer();
+
+    // Hide the start button.
+    questionNumber = 0;
+    time = quizQuestions.length * 15;
+    makeQuestion();
+});
+
+function addScore () {
+    userNameInput = document.getElementById("userName").value
+
+
+
+var newScore = {
+        name: userNameInput,
+        score: secondsLeft
+    };    
+
+    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+
+    /// *******Should be able to push into array *******
+    highScores.push(newScore)
+    ///// **********
+    localStorage.setItem("highScores", JSON.stringify(highScores));
 
 }
 
-//submit button
+
+function hideplayerNote(){
+    var pElement = document.getElementsByClassName("playerNote")[0]
+    pElement.style.display='style'
+}
+
+
+
+choices.addEventListener("click", function (event) {
+    var pElement = document.getElementsByClassName("playerNote")[0]
+
+    ///// ANSWER FUNCTION
+    if (answer === event.target.textContent) {   
+        pElement.innerHTML = "CORRECT!";
+        questionNumber++;
+        makeQuestions();
+        
+    } else {
+        pElement.innerHTML = "INCORRECT.";
+        time = time - 5;
+        questionNumber++;
+        makeQuestions()
+
+    }    
+   
+});
+
+
+
+
+
+
+    //submit button
 //input initials
 
 
@@ -188,7 +264,6 @@ function checkAnswer(event) {
  
 
 //  <!-- GIVEN I am taking a code quiz
-XX
 //  THEN a timer starts and I am presented with a question
 //  WHEN I answer a question
 //  THEN I am presented with another question
@@ -212,4 +287,39 @@ XX
  
 //  * The URL of the GitHub repository. Give the repository a unique name and include a README describing the project. -->
  
- 
+ /// original variables Declaration
+
+ // var startBtn = document.getElementById("startButton");
+// var submitBtn = document.querySelector("submit");
+// var saveBtn = document.querySelector("#save");
+// var timerEl = document.getElementById("timer");
+
+// var time = questions.length * 60;  // Assumes 10 seconds per ques.
+
+// var questionHead = document.getElementById("questions");
+// var answerChoices = document.getElementById("answers"); 
+
+
+// var highScoreClick = document.querySelector("highScoreClick")
+// // Questions Array // Objects: /Choices /Answers \\
+// var questionsForm = document.querySelector("#questionsFrom")
+// var currentQuestion = document.querySelector("#currentQuestion")
+
+// var questionOpt = docuement.querySelector("#questionOpt") //this is the "il"
+
+// var questionIndex = -1; // This may be negative number per *class afterhours notes page12
+// var playerChoice;
+
+
+// ///// HIGH SCORES \\\\\
+// var playerInitials = document.querySelector("#playerInitials");
+// var highScoreLog = document.querySelector("#highScoreLog")
+// var highScores = []; // double check syntax
+// var highScoreForm = document.querySelector("#highScoreForm")
+// var questionBox = document.querySelector("#questionBox")
+// var design2
+
+// var timer = document.getElementById("timer");
+// var currentTimer = 60;
+// var outOfTime = document.querySelector("#outoftime")
+// var score = 0; // Move to timer section 
